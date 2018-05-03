f39f96ec92dabd406e32f84ea0977f58074758f3
Hadi Shad <hadi@mrhadi.com>
2018-05-03 18:32:41 +1000

    Add CORS support

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('I&Co TV Content Management');
 });
